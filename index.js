d50265f5361b31f3d18c0092ca203be45e0ec925
Adrian <adrianshisia@gmail.com>
2023-06-18 10:32:49 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
+const { highlightTags } = require("mocha/lib/utils");
+
 // Write your solution in this file!
+const employee = {
+    name : "Sam",
+    streetAddress : "11 Broadway"
+}
+
+function updateEmployeeWithKeyAndValue(Object,key,value){
+    return{
+    ...Object,
+    [key]:value,
+    }
+}
+const newEmployee = updateEmployeeWithKeyAndValue(
+    employee,
+    "streetAddress",
+    "35 lane"
+)
+
+function destructivelyUpdateEmployeeWithKeyAndValue(Object,key,value){
+    Object[key] = value
+    return Object
+}
+   const Overemployees = destructivelyUpdateEmployeeWithKeyAndValue(
+    employee,
+     "streetAddress",
+     "12 Broadway")
+
+
+function deleteFromEmployeeByKey(Object,key) {
+
+}
